d6c64d13b8c6b2de89b52501c27b164a545b296b
effuone <effuone>
2022-07-22 13:36:13 +0600

    Project Setup

---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,8 @@
+const Login = () => {
+    return(
+        <div className="login_page">
+
+        </div>
+    )
+}
+export default Login
---
+++ src/components/pages/Register.jsx
@@ -0,0 +1,8 @@
+const Register = () => {
+    return(
+        <div className="register_page">
+            
+        </div>
+    )
+}
+export default Login
